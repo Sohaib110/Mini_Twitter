e285002df18536e13370f9a9b5b289c19a4b53af
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-01-22 22:39:22 +0500

    commit

--- twitter/views.py
+++ twitter/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404
 
 def index(request):
     return render(request, 'index.html')
+
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+

e631b9ad8b36ca3dacebc5956185cc63675b075e
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-01-24 23:10:15 +0500

    commits

--- twitter/views.py
+++ twitter/views.py
@@ -24,10 +24,24 @@ def tweet_create(request):
         return render(request, 'tweet_form.html', {'form': form})
     
     
-def tweet_edit(request):
-    if:
-        pass
+def tweet_edit(request, tweet_id):
+    tweet=get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
     else:
-        form = TweetForm()
+        form = TweetForm(instance=tweet)
+        return render(request, 'tweet_form.html', {'form': form})
+
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+      tweet.delete()
+      return redirect('tweet_list')
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
 
 

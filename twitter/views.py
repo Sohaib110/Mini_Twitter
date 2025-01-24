ce15b92aee08fa35e24a395c4ae549a0d89eb6be
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-01-24 22:40:48 +0500

    commit

--- twitter/views.py
+++ twitter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404
@@ -10,5 +10,24 @@ def index(request):
 def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
+
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+           tweet= form.save(commit=False)
+        tweet.user = request.user 
+        tweet.save()
+        return redirect('tweet_list')
+    else:
+        form = TweetForm()
+        return render(request, 'tweet_form.html', {'form': form})
+    
+    
+def tweet_edit(request):
+    if:
+        pass
+    else:
+        form = TweetForm()
 
 
